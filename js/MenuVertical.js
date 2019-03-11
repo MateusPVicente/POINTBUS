7d3947aa7e052e36bb8304809953ef7aa60e08ef
MateusPVicente <42739348+MateusPVicente@users.noreply.github.com>
2019-03-11 13:50:18 -0300

    Hamburguer 2

---
+++ js/MenuVertical.js
@@ -0,0 +1,50 @@
+//////////////////////////////////////////////////////////////////
+//Metodo de manutencao de abertura e fechamento do menu vertical
+//////////////////////////////////////////////////////////////////
+
+var mostrar = true;
+document.getElementById("telaEscura").style.visibility = "hidden";
+
+$(document).ready(function(){
+    $('.icon').click(function(){
+        $('.icon').toggleClass('active');
+    });
+});
+
+document.getElementById("mostrarMenu").onclick = function(){
+    if (mostrar)
+    {
+        //mostrando
+        document.getElementById("menuV").style.left = 0;
+        document.getElementById("telaEscura").style.visibility = "initial";
+    }
+    else
+    {
+        //escondido
+        document.getElementById("menuV").style.left = '-30em';
+        document.getElementById("telaEscura").style.visibility = "hidden";
+    }
+    mostrar = !mostrar;
+}
+
+// $(document).ready(function(){
+//     $('telaEscura').click(function(){
+//         // $('.icon').toggleClass('inactive');
+//         $('#menuV').css('left', '-30em');
+//         $('#telaEscura').css('visibilty', 'hidden');
+//         mostrar = !mostrar;
+
+//     });
+// });
+
+document.getElementById("telaEscura").onclick = function()
+{
+    document.getElementById("menuV").style.left = '-30em';
+    document.getElementById("telaEscura").style.visibility = "hidden";
+    mostrar = !mostrar;
+
+    var menu = document.querySelector('.icon')
+    menu.classList.toggle('active');
+}
+
+//////////////////////////////////////////////////////////////////
