e4d1be500f57f423cd1d1c01c33ce47d50e39ba7
MateusPVicente <42739348+MateusPVicente@users.noreply.github.com>
2019-05-13 10:59:39 -0300

    pesquisa

--- ConsultaBD.sql
+++ ConsultaBD.sql
@@ -14,3 +14,23 @@ cepDestino char(8) not null,
 cepOrigem char(8) not null,
 data date not null
 )
+
+create table Pesquisa(
+ID int primary key identity,
+total int not null,
+qtd1 int not null,
+qtd2 int not null,
+qtd3 int not null,
+qtd4 int not null,
+qtd5 int not null,
+qtd6 int not null,
+)
+select * from Pesquisa
+
+insert into Pesquisa values(0,0,0,0,0,0,0)
+
+create trigger p_tg
+on Pesquisa
+instead of insert
+as
+update Pesquisa set total = Pesquisa.total+1, qtd1 = inserted.qtd1+Pesquisa.qtd1, qtd2 = inserted.qtd2+Pesquisa.qtd2, qtd3 = inserted.qtd3+Pesquisa.qtd3, qtd4 = inserted.qtd4+Pesquisa.qtd4, qtd5 = inserted.qtd5+Pesquisa.qtd5, qtd6 = inserted.qtd6+Pesquisa.qtd6 from inserted where Pesquisa.ID=1
